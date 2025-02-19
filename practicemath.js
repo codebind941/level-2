65fd9604e9ccc016fb45b9936ebb28605d39b382
Syed Saqib <soharwardiandco27@gmail.com>
2025-02-19 23:24:08 +0530

    various math methods

---
+++ practicemath.js
@@ -0,0 +1,27 @@
+function mathmethods(value) {
+let rounded = Math.round(value)
+console.log(rounded)
+
+let ceiling = Math.ceil(value)
+console.log(ceiling)
+
+let flooring = Math.floor(value)
+console.log(flooring)
+
+let randomvalue = Math.random()
+console.log(randomvalue)
+
+let maxvalue = Math.max(2, 4, 3)
+console.log(maxvalue)
+
+let minvalue = Math.min(2, 4, 3)
+console.log(minvalue)
+
+let powvalue = Math.pow(value, 3)
+console.log(powvalue)
+
+let sqrtvalue = Math.sqrt(value)
+console.log(sqrtvalue)
+}
+
+mathmethods(4.57)
